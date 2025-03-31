545456c20d11a3e97a3ec3a9a77aa1fc283b010e
G2-Games <ke0bhogsg@gmail.com>
2025-03-31 17:11:54 -0500

    Fixed some issues

--- main.py
+++ main.py
@@ -4,62 +4,60 @@ from zoneinfo import ZoneInfo
 import requests
 import json
 
+from rotator import Rotator
+
 # Load a timescale
 timescale = load.timescale()
 
-# Get the satellite data from celestrak if it is out of date
-#
-# Only query if the data is > 2 hours old
-# satellite_data_raw = requests.get("https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=JSON")
-# with open(filename, 'wb') as fd:
-#     for chunk in r.iter_content(chunk_size=128):
-#         fd.write(chunk)
-# satellite_data_raw = requests.get("https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=JSON")
-# satellite_data = json.loads(satellite_data_raw.text)
-# satellite_data = list(filter(lambda s : "NOAA-18" in s["OBJECT_NAME"], satellite_data))
-#
-# if len(satellite_data) == 0:
-#     print("No satellites matching name filter found!")
-#     exit(1)
-#
-# # Print the found objects by name
-# for sat in satellite_data:
-#     print(sat["OBJECT_NAME"])
-
+# The position of Lincoln, NE (roughly)
 lincoln = wgs84.latlon(+40.806862, -96.681679)
 
+# Load the TLE
 line1 = "1 90000U 25090A   25091.17708220 -.00012090  00000-0 -25444-3 0    64"
 line2 = "2 90000  90.0015 318.6050 0001128  47.8434 312.3114 15.45593922    34"
 satellite = EarthSatellite(line1, line2, "Fram2", timescale)
 difference = satellite - lincoln
 
-t0 = timescale.utc(2025, 3, 31, 1, 47)
-t1 = t0 + 4
-t, events = satellite.find_events(lincoln, t0, t1, altitude_degrees=15.0)
-event_names = 'rise', 'culminate', 'set'
+# Set up and find passes
+# t0 = timescale.utc(2025, 4, 1, 1, 47)
+# t1 = t0 + 3.7
+# t, events = satellite.find_events(lincoln, t0, t1, altitude_degrees=0.0)
+# event_names = 'rise', 'culminate', 'set'
+#
+# print("Passes starting:")
+# for ti, event in zip(t, events):
+#     if event != 0:
+#         continue
+#
+#     name = event_names[event]
+#     print(ti.astimezone(ZoneInfo("America/Chicago")))
 
-print("Passes starting:")
-for ti, event in zip(t, events):
-    if event != 0:
-        continue
+new_rotator = Rotator("/dev/ttyUSB0")
 
-    name = event_names[event]
-    print(ti.astimezone(ZoneInfo("America/Chicago")))
+while True:
+   sleep(0.1)
 
+   t = timescale.now()
+   geocentric = satellite.at(t)
+   lat, lon = wgs84.latlon_of(geocentric)
 
+   topocentric = difference.at(t)
+   alt, az, distance = topocentric.altaz()
 
-def track_realtime():
-     while True:
-        t = timescale.now()
-        geocentric = satellite.at(t)
-        lat, lon = wgs84.latlon_of(geocentric)
+   azimuth = az.degrees
+   if azimuth > 180:
+      azimuth = azimuth - 360
 
-        topocentric = difference.at(t)
-        alt, az, distance = topocentric.altaz()
+   print('-----')
+   print('Latitude:', lat.degrees)
+   print('Longitude:', lon.degrees)
+   print(f"Altitude: {alt.degrees}")
+   print(f"Azimuth: {azimuth}")
 
-        print('-----')
-        print('Latitude:', lat.degrees)
-        print('Longitude:', lon.degrees)
-        print(f"Altitude: {alt.degrees}")
-        print(f"Azimuth: {az.degrees}")
-        sleep(0.1)
+   print(new_rotator.position())
+
+   if alt.degrees <= 0:
+      continue
+
+   new_rotator.set_position_horizontal(-azimuth)
+   new_rotator.set_position_vertical(alt.degrees)
---
+++ rotator.py
@@ -0,0 +1,69 @@
+import serial
+
+class Rotator():
+    """ A rotator, utilizing the protocol specified here:
+     https://github.com/unl-rocketry/tracker-embedded/blob/main-rust/PROTOCOL.md """
+
+
+    def __init__(self, port: str, baud: int = 115200):
+        # The default timeout here is 2 seconds, is that good?
+        self.main_port = serial.Serial(port, baud, timeout = 2)
+
+
+    def set_position_vertical(self, pos: float):
+        self.main_port.write("DVER {}\n".format(pos).encode())
+
+        self.main_port.readline()
+        result = self.main_port.readline()
+
+    def set_position_horizontal(self, pos: float):
+        self.main_port.write("DHOR {}\n".format(pos).encode())
+
+        self.main_port.readline()
+        result = self.main_port.readline()
+
+    def calibrate_vertical(self, set = False):
+        """ Begin vertical calibration. This function blocks until calibration
+        has finished, which can take a very long time"""
+        if set:
+            self.main_port.write(b"CALV SET\n")
+        else:
+            self.main_port.write(b"CALV\n")
+
+        self.main_port.readline()
+        result = self.main_port.readline()
+
+    def calibrate_horizontal(self):
+        self.main_port.write(b"CALH\n")
+
+        self.main_port.readline()
+        result = self.main_port.readline()
+
+    def move_vertical(self, steps: int):
+        self.main_port.write("MOVV {}\n".format(steps).encode())
+
+        self.main_port.readline()
+        result = self.main_port.readline()
+
+    def move_horizontal(self, steps: int):
+        self.main_port.write("MOVH {}\n".format(steps).encode())
+
+        self.main_port.readline()
+        result = self.main_port.readline()
+
+    def position(self) -> tuple[float, float]:
+        self.main_port.write(b"GETP\n")
+
+        self.main_port.readline()
+        result = self.main_port.readline().split()
+
+        if len(result) < 3:
+            return (0.0, 0.0)
+
+        return (float(result[1]), float(result[2]))
+
+
+
+
+
+
